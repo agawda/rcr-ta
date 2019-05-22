8801d0e53566949b68f42b1f4fe512ed6e8ab3c7
Anna Gawda <a.m.gawda@gmail.com>
2019-05-22 21:49:09 +0200

    Initial commit

---
+++ src/main/java/com/gawda/rcrta/RcrTaApplication.java
@@ -0,0 +1,13 @@
+package com.gawda.rcrta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcrTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcrTaApplication.class, args);
+	}
+
+}
